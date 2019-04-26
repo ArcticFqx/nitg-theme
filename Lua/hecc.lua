210b4a9ea788f539c4c5324100dcea99ba5382e8
ArcticFqx <arcticfqx@afqx.net>
2019-04-26 02:33:07 +0200

    I love video games

--- Lua/hecc.lua
+++ Lua/hecc.lua
@@ -17,7 +17,8 @@ local hecc = {
     "MAX300 Simulator",
     "O-oooooooooo AAAAE-A-A-I-A-U- JO-oooooooooooo AAE-O-A-A-U-U-A- E-eee-ee-eee AAAAE-A-E-I-E-A- JO-ooo-oo-oo-oo EEEEO-A-AAA-AAAA",
     "osu!",
-    "Powered by Ligma"
+    "Powered by Ligma",
+    "I love video games. I REALLY love video games, like a lot! Like a WHOLE LOT! You have no video games."
 }
 
 math.randomseed(tonumber(os.date("%S")))
