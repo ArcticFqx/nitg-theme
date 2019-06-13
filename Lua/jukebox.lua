f7e73c5a93e6e31bc70a983be1caa3ca9197b8df
ArcticFqx <arcticfqx@afqx.net>
2019-06-13 10:51:38 +0200

    F4 is not a char anymore

--- Lua/jukebox.lua
+++ Lua/jukebox.lua
@@ -86,7 +86,7 @@ local function update()
 end
 
 event.Persist("overlay update", "jukebox", update)
-event.Persist("key char", "jukebox", function(c) 
+event.Persist("key func", "jukebox", function(c) 
     if c == "F4" then 
         jukebox.NextSong() 
         SCREENMAN:SystemMessage("Skipping song")
